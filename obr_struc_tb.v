94037855c29d647151a36889b663cfb629973752
AnkitaMoholkar <ankita.moholkar@sjsu.edu>
2021-03-21 00:04:44 -0700

    Update obr_struc_tb.v

--- obr_struc_tb.v
+++ obr_struc_tb.v
@@ -1,1 +1,31 @@
-m
+// Code your testbench here
+// or browse Examples
+module tb;
+  wire out;
+  reg in,clk,we; 
+  
+  lab33 l1 (in,we,clk,out);
+  
+  initial begin 
+    clk=0;
+    forever #5 clk=~clk;
+  end 
+  
+  initial begin 
+    in=0; we=1;
+    #10 in=1;
+    #10 in=0; 
+    #10 in=1;
+    #10 in=0;
+    we=0;
+    #10 in=1;
+    #10 in=0; 
+    #20 $finish;
+  end
+  
+  initial begin 
+    $dumpfile ("new.vcd");
+    $dumpvars;
+  end
+  
+endmodule
