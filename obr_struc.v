e9f92906f433993c4b2cb52cea099a27f0e2d933
AnkitaMoholkar <ankita.moholkar@sjsu.edu>
2021-03-21 00:04:16 -0700

    Update obr_struc.v

--- obr_struc.v
+++ obr_struc.v
@@ -1,1 +1,20 @@
-m
+  module lab33(in,we,clk,out);
+  input clk,in,we;
+  output reg out;
+  
+    and (a1,in,we);
+    and (a2, out, ~we);
+    or (d,a1,a2);
+    
+  notif0 (n1,d,clk);
+  not (n2, n1);
+  notif1 (n1, n2, clk);
+  notif1 (n3, n2, clk);
+  not(n4, n3); 
+  notif1 (n3, n2, clk);
+  not (n4, n3); 
+  notif0 (n3,n4,clk);
+    buf (out,n4); 
+
+  
+endmodule 
