1c271322ccb09baf5c1c0c52563443d4de7d0f6a
AnkitaMoholkar <ankita.moholkar@sjsu.edu>
2021-03-20 23:12:28 -0700

    Create obr_beh_tb.v

---
+++ obr_beh_tb.v
@@ -0,0 +1,31 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+  wire out;
+  reg in,clk,we; 
+  
+  lab33 l1 (in,we,clk,out);
+  
+  initial begin 
+    clk=0;
+    forever #5 clk=~clk;
+  end 
+  
+  initial begin 
+    reset=0; we=1;
+    #10 in=1;
+    #10 in=0; 
+    #10 in=1;
+    #10 in=0;
+    we=0;
+    #10 in=1;
+    #10 in=0; 
+    #20 $finish;
+  end
+  
+  initial begin 
+    $dumpfile ("new.vcd");
+    $dumpvars;
+  end
+  
+endmodule
